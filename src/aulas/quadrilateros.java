0949a8fee2b3d83ca2a339c78985afb02d8d964e
Davy-Geyson <123582957+Davy-201@users.noreply.github.com>
2023-01-27 22:57:08 -0300

    first commit

---
+++ src/aulas/quadrilateros.java
@@ -0,0 +1,19 @@
+package aulas;
+
+public class quadrilateros {
+    public static double area(double lado) {
+        return lado * lado;
+    }
+
+    public static void area(double lado1, double lado2) {
+        System.out.println("area do retangulo: " + lado1 * lado2);
+    }
+
+    public static void area(double baseMaior, double baseMenor, double altura) {
+        System.out.println("area do trapezio: " + ((baseMaior + baseMenor) * altura) / 2);
+    }
+
+    public static void area(Float diagonal1, Float diagonal2) {
+        System.out.println("area do losangulo: " + (diagonal1 * diagonal2) / 2);
+    }
+}
