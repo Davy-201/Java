c24cdee9d80a75e12c2ca6e4b9a6de4b33594d10
Davy-Geyson <123582957+Davy-201@users.noreply.github.com>
2023-02-02 21:38:42 -0300

    first commit

---
+++ src/exercicio/collections/set/ExSet.java
@@ -0,0 +1,73 @@
+package exercicio.collections.set;
+
+public class ExSet {
+    public static void main(String[] args) {
+        
+    }
+}
+class Serie {
+    String nome;
+    String genero;
+    Integer tempoEpisodio;
+
+    public Serie(String nome, String genero, Integer tempoEpisodio) {
+        this.nome = nome;
+        this.genero = genero;
+        this.tempoEpisodio = tempoEpisodio;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public Integer getTempoEpisodio() {
+        return tempoEpisodio;
+    }
+
+    @Override
+    public String toString() {
+        return "[genero=" + genero + ", nome=" + nome + ", tempoEpisodio=" + tempoEpisodio + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((genero == null) ? 0 : genero.hashCode());
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((tempoEpisodio == null) ? 0 : tempoEpisodio.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Serie other = (Serie) obj;
+        if (genero == null) {
+            if (other.genero != null)
+                return false;
+        } else if (!genero.equals(other.genero))
+            return false;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (tempoEpisodio == null) {
+            if (other.tempoEpisodio != null)
+                return false;
+        } else if (!tempoEpisodio.equals(other.tempoEpisodio))
+            return false;
+        return true;
+    }
+    
+}
