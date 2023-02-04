a5a1d06dbe24553ee775286f9134213a5ddb9013
Davy-Geyson <123582957+Davy-201@users.noreply.github.com>
2023-02-04 14:05:01 -0300

    first commit

---
+++ src/exercicio/herança/Filha2.java
@@ -0,0 +1,19 @@
+package exercicio.herança;
+
+public class Filha2 extends ClasseMae {
+
+    @Override
+    public void metodo1() {
+        System.out.println("metodo 1 da classe filha 2 POLIMORFISMO");
+    }
+
+    @Override
+    public void metodo2() {
+        System.out.println("metodo 2 da classe filha 2 POLIMORFISMO");
+    }
+
+    @Override
+    public void metodo3() {
+        System.out.println("metodo 3 da classe filha 3 SOBRESCRITA");
+    }
+}
