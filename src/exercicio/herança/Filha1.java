13c7094a7e3d438f6a304f8a8bc7c9bef6be59e0
Davy-Geyson <123582957+Davy-201@users.noreply.github.com>
2023-02-04 14:04:17 -0300

    first commit

---
+++ src/exercicio/herança/Filha1.java
@@ -0,0 +1,9 @@
+package exercicio.herança;
+
+public class Filha1 extends ClasseMae {
+
+    @Override
+    public void metodo1() {
+        System.out.println("metodo 1 da classe filha 1 POLIMORFISMO");
+    }
+}
