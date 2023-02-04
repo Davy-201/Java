6a300a5fc51b4dd6e89e3648115e8453b82c9c88
Davy-Geyson <123582957+Davy-201@users.noreply.github.com>
2023-02-04 14:00:35 -0300

    first commit

---
+++ src/exercicio/herança/ClasseMae.java
@@ -0,0 +1,16 @@
+package exercicio.herança;
+
+public class ClasseMae {
+
+    public void metodo1() {
+        System.out.println("metodo 1 classe mãe");
+    }
+
+    public void metodo2() {
+        System.out.println("metodo 2 classe mãe");
+    }
+
+    public void metodo3() {
+        System.out.println("metodo 3 classe mãe");
+    }
+}
