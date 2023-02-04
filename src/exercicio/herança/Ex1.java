c1e3f305a97f4205b54f3eeebc3302dc9931e335
Davy-Geyson <123582957+Davy-201@users.noreply.github.com>
2023-02-04 14:03:12 -0300

    first commit

---
+++ src/exercicio/herança/Ex1.java
@@ -0,0 +1,15 @@
+package exercicio.herança;
+
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String nome;
+        int idade;
+
+        System.out.println("nome: ");
+        System.out.println("idade: ");
+    }
+}
