ea3dc48b6e1e5081aa94e694eeb12990bbfbda3a
Davy-Geyson <123582957+Davy-201@users.noreply.github.com>
2023-02-07 23:29:22 -0300

    first commit

---
+++ src/exercicio/teste/OperacaoMatematica.java
@@ -0,0 +1,12 @@
+package exercicio.teste;
+
+public interface OperacaoMatematica {
+
+    void soma(int valor1, int valor2);
+    
+    void subtracao(int valor1, int valor2);
+    
+    void divisao(int valor1, int valor2);
+
+    void multiplicacao(int valor1, int valor2);
+}
